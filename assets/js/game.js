9932d85a16fb51affeca3f7cda61b662d6108d4f
Max Fathauer <maxfathauer@gmail.com>
2022-02-26 18:08:21 -0500

    added initial javascript to game

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,9 @@
-window.alert("Javascript is running");
+ /*variable player name */
+ var playerName = window.prompt("What is your robot's name?");
+ 
+//fight();
+//this creates a function called fight
+function fight(){
+    window.alert("The fight has begun!");
+}
+
